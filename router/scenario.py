863f0c98f5052e61a544fc25b117b4810b0865a3
Tim <tim.wuethrich@stud.unibas.ch>
2025-02-07 13:00:11 +0100

    clean scenario

--- router/scenario.py
+++ router/scenario.py
@@ -2,8 +2,9 @@
 
 from pydantic import BaseModel
 from fastapi import APIRouter
-from typing import ClassVar, Optional
+from typing import ClassVar, List, Optional
 from generalClasses.monthYear import *
+from router.person import Person
 
 
 class Scenario(BaseModel):
@@ -15,58 +16,10 @@ class Scenario(BaseModel):
     endDate: ClassVar[MonthYear] = get_lastYearLastMonth
 
 
-# Planninghorizon for all scenarios identical
-baseDate = get_lastYearLastMonth
-endDate = get_lastYearLastMonth
 
-#Dictionary for managing all scenario position
-scenarioDic = {}
 
 #starting router
 router = APIRouter(prefix="/scenario", tags=["scenario"])
 
-# Returs basedate
-@router.post("/get-baseDate/")
-def get_baseDate():
-    return baseDate
-
-#creating a new scenario-object
-@router.post("/create-scenario/{scenario_id}")
-def create_scenario(scenario_id: int, scenario: Scenario):
-    if scenario_id in scenarioDic:
-        return {"Error": "scenario_id already used"}
-    
-    scenarioDic[scenario_id] = scenario
-    return scenarioDic[scenario_id]
-
-# Changes on existing scenario-object
-@router.put("/update-scenario/{scenario_id}")
-def update_scenario(scenario_id: int, scenario: Scenario):
-    if scenario_id not in scenarioDic:
-        return {"Error": "scenario_id not found"}
-    
-    scenarioDic[scenario_id].update(scenario)
-    return scenarioDic[scenario_id]
-
-# Deleting an existing scenario object
-@router.delete("/delete-scenario/{scenario_id}")
-def delete_scenario(scenario_id: int):
-    if scenario_id not in scenarioDic:
-        return {"Error": "scenario_id not found"}
-    
-    del scenarioDic[scenario_id]
-    return {"Success": "Scenario deleted"}
-
-# Returns scenario position by id
-@router.get("/get-scenario/{scenario_id}")
-def get_scenario(scenario_id: int):
-    if scenario_id not in scenarioDic:
-        return {"Error": "scenario_id not found"}
-    return scenarioDic[scenario_id]
-
-# Returns all Scenarios
-@router.get("/get-allscenarios/")
-def get_allscenarios():
-    return scenarioDic
 
 
