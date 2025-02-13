486d1b177b5df239ecaddc25be21b377e0d53a13
Tim <tim.wuethrich@stud.unibas.ch>
2025-02-13 09:33:49 +0100

    working on taxes

--- backend/tax/BE/incometaxBE.py
+++ backend/tax/BE/incometaxBE.py
@@ -4,7 +4,7 @@
 import os
 from backend.tax.taxproperties import CivilStatus, Confession
 import csv
-import pandas as pd
+
 
 
 srcledig = None
@@ -15,21 +15,7 @@ def clc_incomeTax(income: float, conf: Confession, civ: CivilStatus) -> "float":
     return 0
 
 def clc_einfSteuer(income: float, conf: Confession, civ: CivilStatus) -> "float":
-    if civ == CivilStatus.alleinstehend:
-        
-        if srcalleinstehend is None:
-            file_path = os.path.join("src", "BE", "BE_alleinst_Einkommen.csv")
-            srcalleinstehend = pd.read_csv(file_path)
-        
-    src = srcalleinstehend
+    return 0
 
-    if civ == CivilStatus.verheiratet:
-        
-        if srcalleinstehend is None:
-            file_path = os.path.join("src", "BE", "BE_verh_Einkommen.csv")
-            srcalleinstehend = pd.read_csv(file_path)
-        
-    src = srcalleinstehend
 
-    
 
